c3d07ad9e0f514eaa5456828f280d35d7546db67
Stephen <895745843@qq.com>
2018-03-22 20:18:16 +0800

    新增：状态栏样式

---
+++ library-taihuoniao/src/main/java/com/stephen/taihuoniaolibrary/utils/THNStatusBarUtils.java
@@ -0,0 +1,45 @@
+package com.stephen.taihuoniaolibrary.utils;
+
+import android.app.Activity;
+import android.os.Build;
+import android.view.WindowManager;
+
+import com.readystatesoftware.systembartint.SystemBarTintManager;
+
+/**
+ * Created by taihuoniao on 2016/4/14 19:19
+ * Email: 895745843@qq.com
+ */
+public class THNStatusBarUtils {
+    //状态栏变为黑色
+    public static void chenjin(Activity activity, int colorId) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            SystemBarTintManager tintManager = new SystemBarTintManager(activity);
+            tintManager.setStatusBarTintColor(activity.getResources().getColor(colorId));
+            tintManager.setStatusBarTintEnabled(true);
+        }
+    }
+
+    //透明状态栏
+    public static void showStatusBar(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            //透明状态栏
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+    }
+
+    //隐藏状态栏
+    public static void hide(Activity activity) {
+        WindowManager.LayoutParams attrs = activity.getWindow().getAttributes();
+        attrs.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
+        activity.getWindow().setAttributes(attrs);
+    }
+
+    //显示状态栏
+    public static void show(Activity activity) {
+        WindowManager.LayoutParams attrs = activity.getWindow().getAttributes();
+        attrs.flags &= ~WindowManager.LayoutParams.FLAG_FULLSCREEN;
+        activity.getWindow().setAttributes(attrs);
+    }
+}
